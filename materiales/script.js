0e91c5bd239b4533fe50a9acad244289873bb53d
arieldiaz11777 <arieldiazapp1177@gmail.com>
2025-01-08 16:06:11 +0000

    ok

--- materiales/script.js
+++ materiales/script.js
@@ -1,6 +1,7 @@
 document.getElementById('materialForm').addEventListener('submit', async (e) => {
     e.preventDefault();
 
+    // Obtener los datos del formulario
     const data = {
         curso: document.getElementById('curso').value,
         numeroClase: document.getElementById('numeroClase').value,
@@ -11,15 +12,17 @@ document.getElementById('materialForm').addEventListener('submit', async (e) => 
     };
 
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbyLvsMUTRK40cXBWfU_856cMIpOhLsCfLdBIjwzkiA3QBGtEBqTIX2-BZPTc4DUdnGW/exec', {
+        // Enviar los datos al Web App del App Script
+        const response = await fetch('https://script.google.com/macros/s/AKfycbyKs1TtpknpMDzDhv_K050uOL6gXZyrotyvtobqIhJfHzixJ1HvtvZuyMCIvo3Hq1ra/exec', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data),
         });
 
         const result = await response.json();
+
         if (result.status === 'success') {
-            alert('Datos enviados correctamente');
+            alert('Datos enviados correctamente a Google Sheets');
         } else {
             alert('Error al enviar los datos');
         }
